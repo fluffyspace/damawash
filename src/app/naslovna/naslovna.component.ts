a07576dfb7eb428f2c5054c6211c9be101fd8ddf
Ingo Kodba <ingokodba@gmail.com>
2022-07-07 12:52:12 +0200

    dodane druge slike, cjenik updatean

--- src/app/naslovna/naslovna.component.ts
+++ src/app/naslovna/naslovna.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-naslovna',
@@ -9,7 +10,22 @@ export class NaslovnaComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('firstimage') image: ElementRef<HTMLInputElement> | undefined;
+  
+  images: string[] = ['before_after1.jpg', 'before_after2.jpg', 'before_after3.jpg'];
+
   ngOnInit(): void {
+    interval(3000).subscribe(() => this.carousel())
+  }
+
+  slideIndex = 0;
+
+  carousel(): void {
+    this.slideIndex++;
+    if (this.slideIndex >= 3) {this.slideIndex = 0}
+    if(this.image){
+      this.image.nativeElement.src = 'assets/images/' + this.images[this.slideIndex];
+    }
   }
 
 }
